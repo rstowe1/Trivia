5b675ca9000c57cdaaa928cb394af6cca371cfd6
rstowe1 <ryanwstowe@gmail.com>
2018-03-16 21:46:31 -0400

    pushing all documents at their current status

---
+++ assets/javascript/variables.js
@@ -0,0 +1,32 @@
+
+var answer1 = {
+    Question1 :'Throughout the Ole Miss football history, how many different head coaches have led the team to an SEC Championship?'
+    options : ['1','0','2','3'],
+    answer : '1',
+    userAnswer : '',
+    correctAnswer : ''
+};
+
+var answer2 = {
+    Question2 : 'Since the SEC split into divisions, Ole Miss won a share of the SEC West title in what year?',
+    options : ['2003', '1995', '2006','1922'],
+    answer : '2003',
+    userAnswer : '',
+    correctAnswer : ''
+};
+
+var answer3 = {
+    Queston3 : 'Which Ole Miss QB holds the record for most passing yards in a single game?',
+    options : ['Archie Manning', 'Bo Wallace', 'Eli Manning', 'Kent Austin'],
+    Answer : 'Archie Manning',
+    userAnswer : '',
+    correctAnswer : ''
+};
+
+var answer4 = {
+    Question4 : 'Which Ole Miss QB threw for 6 touchdowns in a single game?',
+    options : ['Eli Manning', 'Archie Manning', 'Shay Hodges', 'Chris Collins']
+    Answer :'Eli Manning',
+    userAnswer : '',
+    correctAnswer: ''
+};
